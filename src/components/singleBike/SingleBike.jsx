7438ac418230e4ad6a0bc53f62428ce44638fcef
Mr. Majedul Islam <majedulislam393@gmail.com>
2022-03-25 22:05:08 +0600

    fake data created done

--- src/components/singleBike/SingleBike.jsx
+++ src/components/singleBike/SingleBike.jsx
@@ -1,12 +1,16 @@
 import "./SingleBike.css";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 const SingleBike = ({ bike }) => {
   const { img, name, price } = bike;
   return (
     <div className="single-bike">
       <img src={img} alt="" />
       <h3>{name}</h3>
-      <h2>{price}</h2>
+      <h2>price: {price}</h2>
+      <span>
+        add to cart <FontAwesomeIcon icon={faShoppingCart} />{" "}
+      </span>
     </div>
   );
 };
