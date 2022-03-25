b6f72cf95994eb51fa763801e29046cabe7ed0d0
Mr. Majedul Islam <majedulislam393@gmail.com>
2022-03-25 22:47:34 +0600

    display fake data done

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -4,6 +4,10 @@ const Cart = () => {
   return (
     <div className="cart">
       <h3>selected bikes</h3>
+      <div className="btn-container">
+        <span>choose 1 for me</span>
+        <span>choose again</span>
+      </div>
     </div>
   );
 };
